7ad8a84c048383702b2a16a9ec3dab2d80ae627a
oscar <oscarmagallon2002@hotmail.com>
2022-09-16 12:57:00 +0200

    inicio Pagos

--- App/controladores/Pagos.php
+++ App/controladores/Pagos.php
@@ -16,7 +16,8 @@
 
 
         public function index(){
-            
+            //conseguir el precio y los articulos de cada moto
+            //SELECT idArticulos from pieza INNER JOIN reparaciones on pieza.idreparaciones = reparaciones.idreparaciones INNER JOIN incidencias ON reparaciones.idreparaciones = incidencias.idreparaciones INNER JOIN moto_has_incidencias on moto_has_incidencias.idIncidencias = incidencias.idIncidencias INNER JOIN moto on moto_has_incidencias.idMoto = moto.idMoto WHERE pieza.idArticulos =;
             }
             
 
