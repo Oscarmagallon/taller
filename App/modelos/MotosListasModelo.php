a3bfbe267a584fffc835ffe37d056d61c2786790
oscar <oscarmagallon2002@hotmail.com>
2022-11-04 12:27:58 +0100

    motosListas, hay que poner una pieza

--- App/modelos/MotosListasModelo.php
+++ App/modelos/MotosListasModelo.php
@@ -14,10 +14,10 @@
             INNER JOIN incidencias on moto_has_incidencias.idIncidencias = incidencias.idIncidencias 
             INNER JOIN reparaciones on reparaciones.idreparaciones = incidencias.idreparaciones 
             INNER JOIN ingreso on ingreso.reparaciones_idreparaciones = reparaciones.idreparaciones 
-            where ingreso.Pagado = 0 Group by incidencias.idIncidencias");
+            where ingreso.Pagado = 0 or reparaciones_idreparaciones not in ( SELECT reparaciones_idreparaciones as SinManoObra FROM `ingreso` WHERE Descr = 'Mano Obra' ) Group by incidencias.idIncidencias");
             return $this->db->registros();
             //quitar
-            "SELECT * FROM `ingreso` WHERE reparaciones_idreparaciones not in ( SELECT reparaciones_idreparaciones as SinManoObra FROM `ingreso` WHERE Descr = 'Mano Obra' );";
+            "SELECT * FROM `ingreso` WHERE ;";
         }
 
 
