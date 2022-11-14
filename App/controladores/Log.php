9b42ac5c5ab6da126061f8e313ce9a97ac9ee69c
oscar <oscarmagallon2002@hotmail.com>
2022-11-14 09:56:39 +0100

    Log al pagar o avisar por emial terminado

--- App/controladores/Log.php
+++ App/controladores/Log.php
@@ -6,5 +6,6 @@
         $logFile = fopen("c:\\log.txt","a");
         fwrite($logFile, "\n".date("d/m/Y H:i:s")." ".$texto) or die("Error escribiendo en el archivo");
         fclose($logFile);
+        return;
         }
 ?>
