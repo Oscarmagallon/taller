324e83b624e9b0cacfd17bd8c50123211b7c4c90
oscar <oscarmagallon2002@hotmail.com>
2022-11-14 10:07:19 +0100

    no poder modificar fecha

--- App/vistas/cliente/incidencias.php
+++ App/vistas/cliente/incidencias.php
@@ -7,15 +7,15 @@
     <form autocomplete="off" action="<?php echo RUTA_URL?>/Incidencias/peticion" method="post" class="card-body">
         <div class="mb-3">
         <label for="tipo">Tipo: <sup>*</sup></label>
-            <input type="text" name="tipo" id="tipo" class="form-control form-control-lg">
+            <input type="text" name="tipo" id="tipo" required class="form-control form-control-lg">
         </div>
         <div class="mb-3">
         <label for="descr">Descripcion:</label>
-            <input type="text" name="descr" id="descr" class="form-control form-control-lg" >
+            <input type="text" name="descr" required id="descr" class="form-control form-control-lg" >
         </div>
         <div class="mb-3">
         <label for="fecha">Fecha:</label>
-            <input type="Date" name="fecha" id="fecha" class="form-control form-control-lg">
+            <input type="text" name="fecha" id="fecha"  value = "<?php echo date('d/m/Y') ?>" disabled r class="form-control form-control-lg">
         </div>
         <input type="hidden" name="idMoto" id="idMoto" class="form-control form-control-lg" value="<?php echo $datos['id']?>">
         <input type="submit" class="btn btn-success" value="Hacer Peticion">
